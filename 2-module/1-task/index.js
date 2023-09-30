c9b21fbf6e25e4d24a410b740ccba50b69f5ffab
Dmitry Verevkin <dim-verevkin@yandex.ru>
2023-09-30 13:59:43 +0300

    Testing task from module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let total = 0;
+  for (let key in salaries) {
+    if ((typeof salaries[key] == 'number') && isFinite(salaries[key])){
+      total += salaries[key];
+    }
+  }
+  return total;
 }
